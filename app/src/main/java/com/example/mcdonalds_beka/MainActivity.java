25ff170f56d7916c77cfa72225d1e84d87a6f9f9
senai <senai@iMac-Aluno-12.local>
2026-08-25 16:59:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mcdonalds_beka/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mcdonalds_beka;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Pega a imagem pelo ID
+        ImageView imagem = findViewById(R.id.imageView);
+
+        // Quando clicar na imagem, vai para a Home
+        imagem.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,home_mc.class);
+            startActivity(intent);
+        });
+    }
+}
